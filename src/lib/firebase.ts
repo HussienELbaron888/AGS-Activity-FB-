f22faa7c3fef5203190ced9b045329049f38d0e2
Hussien ELbaron <hussienelbaron888@gmail.com>
2025-09-09 11:00:13 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -8,13 +8,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "REPLACE_WITH_YOUR_API_KEY",
-  authDomain: "REPLACE_WITH_YOUR_AUTH_DOMAIN",
-  projectId: "REPLACE_WITH_YOUR_PROJECT_ID",
-  storageBucket: "REPLACE_WITH_YOUR_STORAGE_BUCKET",
-  messagingSenderId: "REPLACE_WITH_YOUR_MESSAGING_SENDER_ID",
-  appId: "REPLACE_WITH_YOUR_APP_ID",
-  measurementId: "REPLACE_WITH_YOUR_MEASUREMENT_ID"
+  "projectId": "ags-activities-hub",
+  "appId": "1:945966511726:web:c768113a13bbce4fde987f",
+  "storageBucket": "ags-activities-hub.firebasestorage.app",
+  "apiKey": "AIzaSyD_b7JSPSLapTa5mlKKVDPGvGOJH-793Rk",
+  "authDomain": "ags-activities-hub.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "945966511726"
 };
 
 // Initialize Firebase
